3bc325fb4f1862c5ee483451626e1a642dfce70c
Felix Wiemuth <felixwiemuth@hotmail.de>
2011-02-09 12:43:54 +0100

    added class "Command"

---
+++ Command.h
@@ -0,0 +1,17 @@
+/* Copyright (C) 2011 by Felix Wiemuth
+   This code is licensed under the GNU GENERAL PUBLIC LICENSE http://www.gnu.org/licenses/gpl.txt */
+
+#ifndef COMMAND_H_INCLUDED
+#define COMMAND_H_INCLUDED
+
+class Command
+{
+    public:
+        Command();
+    private:
+
+    public:
+};
+
+
+#endif // COMMAND_H_INCLUDED
