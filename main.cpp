e4c0ba4db1f0f4539889965a8219322464b67e3e
Felix Wiemuth <felixwiemuth@hotmail.de>
2011-02-09 12:43:24 +0100

    added main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/* Copyright (C) 2011 by Felix Wiemuth
+   This code is licensed under the GNU GENERAL PUBLIC LICENSE http://www.gnu.org/licenses/gpl.txt */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
