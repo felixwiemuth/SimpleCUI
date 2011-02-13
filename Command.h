9561a994c67f64d062544085af190bd772fc7d52
Felix Wiemuth <felixwiemuth@hotmail.de>
2011-02-13 18:03:00 +0100

    now exit-codes on command execution

--- Command.h
+++ Command.h
@@ -21,15 +21,19 @@ class Command
             obj = 0;
             mptr = 0;
             mptr_val = 0;
+            err_msg = msg_not_available.end();
         }
 
         Command(FuncClass* obj, Mptr mptr, Mptr_val mptr_val=0) : obj(obj), mptr(mptr), mptr_val(mptr_val)
         {
-
+            err_msg = msg_not_available.end();
         }
 
 
     private:
+        bool available; //methods in 'mptr' and 'mptr_val' can only be executed if set 'true' -- otherwise '*err_msg' is shown
+        std::vector<std::string> msg_not_available; //container of messages that include reasons why this method is not available
+        std::vector<std::string>::iterator err_msg; //activated not-available-message
         FuncClass* obj; //object to execute function on
         void (FuncClass::*mptr)(); //pointer to function with no parameters
         void (FuncClass::*mptr_val)(std::vector<std::string>); //pointer to function with parameter
@@ -41,32 +45,51 @@ class Command
             *this = other;
             return *this;
         }
+
         Command<FuncClass>& set(FuncClass* obj, Mptr mptr, Mptr_val mptr_val=0) //'set(...)' as "constructor" version
         {
-            this->obj = obj;
-            this->mptr = mptr;
-            this->mptr_val = mptr_val;
-            return *this;
+            return set(Command<FuncClass>(obj, mptr, mptr_val));
         }
 
-        bool execute()
+        int check_valid() //return value: 0 = execution of 'mptr' or 'mptr_val' would be valid (regarding 'available' and 'obj') 1 = not available 2 = obj==0
         {
-            if (obj != 0 && mptr != 0)
+            if (!available)
             {
-                ((*obj).*mptr)();
-                return true;
+                if (err_msg != msg_not_available.end())
+                    std::cout << *err_msg << std::endl;
+                return 1;
             }
-            return false;
+            if (obj == 0)
+            {
+                return 2;
+            }
+            return 0;
         }
 
-        bool execute(std::vector<std::string> values)
+        int execute() //return values: 0-2: 'check_valid()' 3 = no 'mptr' defined
         {
-            if (obj != 0 && mptr_val != 0)
+            int ret = check_valid();
+            if (ret != 0)
+                return ret;
+            if (mptr == 0)
             {
-                ((*obj).*mptr_val)(values);
-                return true;
+                return 3;
             }
-            return false;
+            ((*obj).*mptr)();
+            return 0;
+        }
+
+        int execute(std::vector<std::string> values) //return values: 0-2: 'check_valid()' 4 = no 'mptr_val' defined
+        {
+            int ret = check_valid();
+            if (ret != 0)
+                return ret;
+            if (mptr_val == 0)
+            {
+                return 3;
+            }
+            ((*obj).*mptr_val)(values);
+            return 0;
         }
 };
 
--- Cui.h
+++ Cui.h
@@ -29,7 +29,11 @@ class Cui
             err_wrong_call_2 = "' without parameters!";
             err_wrong_call_p_1 = "Error: No function defined to call '";
             err_wrong_call_p_2 = "' with parameters!";
+            err_no_object_defined_1 = "No object specified to execute '";
+            err_no_object_defined_2 = "' on!";
         }
+
+
     private:
         std::map< std::string, Command<FuncClass> > cmds; //all possible commands
         std::string exit_cmd; //command to leave 'run()' (can be overwritten by commands in 'cmds')
@@ -43,6 +47,10 @@ class Cui
         std::string err_wrong_call_2;
         std::string err_wrong_call_p_1;
         std::string err_wrong_call_p_2;
+        std::string err_no_object_defined_1;
+        std::string err_no_object_defined_2;
+
+
     public:
         void run()
         {
@@ -60,25 +68,48 @@ class Cui
                 if (it != cmds.end())
                 {
                     //words[0] == command -- words[1]...words[n] == values
+                    int execode; //exitcode
+
+                    //call method without/with paramter
                     if (words.size() == 1)
-                    {
-                        if (! it->second.execute() )
-                            std::cout << err_wrong_call_1 << words.front() << err_wrong_call_2 << std::endl;
-                    }
+                        execode = it->second.execute();
                     else
+                        execode = it->second.execute(std::vector<std::string>(words.begin()+1, words.end()));
+
+                    //print error message belonging to exitcode
+                    switch(execode)
                     {
-                        if (! it->second.execute(std::vector<std::string>(words.begin()+1, words.end())))
-                            std::cout << err_wrong_call_p_1 << words.front() << err_wrong_call_p_2 << std::endl;
+                        case 0:
+                            //everything o.k.
+                            break;
+                        case 1:
+                            //message sent by Command if enabled
+                            break;
+                        case 2:
+                            std::cout << err << err_no_object_defined_1 << words.front() << err_no_object_defined_2 << std::endl;
+                            break;
+                        case 3:
+                            std::cout << err << err_wrong_call_1 << words.front() << err_wrong_call_2 << std::endl;
+                        case 4:
+                            std::cout << err << err_wrong_call_p_1 << words.front() << err_wrong_call_p_2 << std::endl;
+                        default:
+                            break;
                     }
                 }
                 else
                 {
-                    if (in == exit_cmd)
+                    if (words.front() == exit_cmd)
                     {
-                        std::cout << msg_exit << std::endl;
-                        return;
+                        if (words.size() == 1)
+                        {
+                            std::cout << msg_exit << std::endl;
+                            return;
+                        }
+                        else
+                            std::cout << err << err_wrong_call_p_1 << words.front() << err_wrong_call_p_2 << std::endl;
                     }
-                    std::cout << err << err_no_command_1 << words.front() << err_no_command_2 << std::endl;
+                    else
+                        std::cout << err << err_no_command_1 << words.front() << err_no_command_2 << std::endl;
                 }
             }
         }
